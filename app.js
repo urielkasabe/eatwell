1cf19b7df86c4a3fb9fb7475de513d8756cb1580
Uriel Kasabe <urielkasabe5@gmail.com>
2023-08-07 10:53:26 +0300

    upload eatwell site

---
+++ app.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    res.render('index');
+})
+
+app.get('/restaurants', function (req, res) {
+    const FilePath = path.join(__dirname, 'data', 'restaurants.json');
+    const fileData = fs.readFileSync(FilePath);
+    const storedRes = JSON.parse(fileData);
+
+    res.render('restaurants', { numOfRestaurants: storedRes.length, restaurants: storedRes });
+});
+
+app.get('/reccomendations', function (req, res) {
+    res.render('recommend');
+
+});
+
+app.post('/reccomendations', function (req, res) {
+    const restaurant = req.body;
+    const FilePath = path.join(__dirname, 'data', 'restaurants.json');
+    const fileData = fs.readFileSync(FilePath);
+    const storedRes = JSON.parse(fileData);
+
+    storedRes.push(restaurant);
+
+    fs.writeFileSync(FilePath, JSON.stringify(storedRes));
+
+    res.redirect('/confirm');
+});
+
+app.get('/confirm', function (req, res) {
+    res.render('confirm');
+
+});
+
+app.get('/about', function (req, res) {
+    res.render('about');
+
+});
+
+
+
+
+
+app.listen(3000);
